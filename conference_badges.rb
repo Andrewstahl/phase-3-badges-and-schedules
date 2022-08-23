69790f79f67308def11924dc7f60649a8281f1f7
Andrew Stahl <andrewstahl96@gmail.com>
2022-08-22 20:20:20 -0400

    commit tree 11bd5c63f6805975903cb615a8ca401a819cb81e

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,32 @@
+require "pry"
+
 # Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  attendees.map{ |name| badge_maker(name) }
+end
+
+def assign_rooms(speakers)
+  return_messages = []
+  speakers.each_with_index do |name, index|
+    # puts badge_maker(name)
+    welcome_message = "Hello, #{name}! You'll be assigned to room #{index + 1}!"
+    return_messages.push(welcome_message)
+    # binding.pry
+  end
+  return_messages
+end
+
+def printer(names)
+  names.each do |name|
+    puts badge_maker(name)
+  end
+  room_assignments = assign_rooms(names)
+  room_assignments.each{ |assignment_message| puts assignment_message }
+end
+
+# speaker_list = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+# puts printer(speaker_list)
